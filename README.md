cafed4e9115b1d309493015db4a98b49fa37d9b2
Claudiu Onica <claudiu.onica@gmail.com>
2018-02-01 11:05:12 -0800

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # logs-analysis
-Views created:
+===
+
+## Description
+logs-analysis is a reporting tool that uses information from 'news' database to answer the folowing questions:
+1. What are the most popular three articles of all time? 
+2. Who are the most popular article authors of all time? 
+3. On which days did more than 1% of requests lead to errors?
+
+## Requirements
+* Python 3
+* psycopg2;
+
+    Psycopg is the most popular PostgreSQL database adapter for the Python programming language. Its main features are the complete implementation of the Python DB API 2.0 specification and the thread safety (several threads can share the same connection). It was designed for heavily multi-threaded applications that create and destroy lots of cursors and make a large number of concurrent INSERTs or UPDATEs. It can be installed using pip:
+
+        `pip install psycopg2`
+    
+
+## Installation
+* Fork and clone the repository https://github.com/udacity/fullstack-nanodegree-vm
+* Clone the repository https://github.com/claudiu-onica/logs-analysis.git into the vagrant folder of your virtual machine
+## Usage
+In a terminal start the VM.
+
+Navigate to "vagrant\LogsanalysisProject" then execute the following command: 
+   
+    `python logs-analysis.py`
+
+
+
+The application uses the following views to calculate the percentage of the failed requests. The views are created by the application:
 
 "create or replace view daily_errors as
     select l.time::date as day, count(l.status) as error_count
--- logs-analysis.py
+++ logs-analysis.py
@@ -1,30 +1,24 @@
-
+#!/usr/bin/env python3
+"""logs-analysis produces reports based on the data from
+    the database 'news'
+"""
 
 import psycopg2
 
+# Define a static variable  the database name
 DBNAME = 'news'
 
 
-def get_posts():
-    """Return all posts from the 'database', most recent first."""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select content, time from posts order by time desc")
-    res = c.fetchall()
-    db.close()
-    return res
-
-
-def add_post(content):
-    """Add a post to the 'database' with the current timestamp."""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("insert into posts values(%s)", (content,))
-    db.commit()
-    db.close()
-
-
 def fetch_data(select_str):
+    """fetch_data function connects the the database, executes
+    the passed in query and returns the results.
+
+    Arguments:
+        select_str {string} -- psql query to be executed
+
+    Returns:
+        [type] -- [description]
+    """
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(select_str)
@@ -33,16 +27,53 @@ def fetch_data(select_str):
     return res
 
 
+def create_view(view):
+    """create_view - creates the views used to calculate the failed requests
+
+    Arguments:
+        view {string} -- psql CREATE VIEW command
+
+    Returns:
+        Boolean -- True if the command succeeded othewise it returns False
+    """
+
+    ret = True
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    try:
+        c.execute(view)
+        db.commit()
+    except:
+        print('Error creating the view!')
+        ret = False
+    db.close()
+    return ret
+
+
 def get_popular_articles():
+    """get_popular_articles returns the most popular three articles of all time
+
+    Returns:
+        string -- formated string containing the name and number of views
+        of the top 3 most popular articles
+    """
+
     query = """select a.title, count(l.path) as num from articles a, log l where l.path
             like format(\'%%%s%%\', a.slug) group by a.title
             order by num desc limit 3"""
     res = fetch_data(query)
-    articles = "\n".join("%s - %s views" % (article, num) for article, num in res)
+    articles = "\n".join("%s - %s views" % (article, num)
+                         for article, num in res)
     return articles
 
 
 def get_popular_authors():
+    """get_popular_authors returns the most popular article authors of all time
+
+    Returns:
+        string -- list of the most popular authors
+    """
+
     query = """select au.name, count(l.path) as num from articles a, log l, authors au
             where a.author = au.id and l.path like format('%%%s%%', a.slug)
             group by au.name order by num desc"""
@@ -52,29 +83,44 @@ def get_popular_authors():
 
 
 def get_most_errors():
-    daily_request_view = """create or replace view daily_errors as
-    select l.time::date as day, count(l.status) as error_count
-    from log l where l.status != '200 OK' group by day order by day"""
+    """get_most_errors returns the days on wich more than 1% of requests
+    lead to errors
 
-    daily_errors_view = """create or replace view daily_requests as
-    select l.time::date as day, count(l.status) as requests
-    from log l group by day order by day"""
+    Returns:
+        string -- formated string containing the date and the percentage
+                    of failed requests
+    """
 
+    errors_view = """create or replace view daily_errors as
+                    select l.time::date as day, count(l.status) as error_count
+                    from log l where l.status != '200 OK'
+                    group by day order by day;"""
+    requests_view = """create or replace view daily_requests as
+                    select l.time::date as day, count(l.status) as requests
+                    from log l group by day order by day;"""
     query = """ select to_char(r.day, 'Month DD, YYYY') as day,
-    to_char(e.error_count*100/r.requests::float, 'FM990D9999') as percent
-    from daily_requests r, daily_errors e where r.day = e.day and
-    e.error_count*100/r.requests::float > 1 order by percent desc"""
-    res = fetch_data(query)
-    articles = "\n".join("%s - %s%% errors" % (article, num) for article, num in res)
-    return articles
+                to_char(e.error_count*100/r.requests::float, 'FM990D9999')
+                as percent
+                from daily_requests r, daily_errors e where r.day = e.day and
+                e.error_count*100/r.requests::float > 1
+                order by percent desc"""
+    if create_view(errors_view) and create_view(requests_view):
+        res = fetch_data(query)
+        articles = "\n".join("%s - %s%% errors" % (article, num)
+                             for article, num in res)
+        return articles
+    else:
+        return "Error creating views!"
 
 
 def print_logs():
-    print("\nThe most popular three articles of all time:")
+    """print_logs prints all three reports
+    """
+    print("\nThe most popular three articles of all time:\n")
     print(get_popular_articles())
-    print("\nThe most popular article authors of all time:")
+    print("\nThe most popular article authors of all time:\n")
     print(get_popular_authors())
-    print("\nDays on wich more than 1% of requests lead to errors:")
+    print("\nDays on wich more than 1% of requests lead to errors:\n")
     print(get_most_errors())
 
 
